3bf3339b2cf0b6be0e17c4a2db7843780c9bf0a5
baggers <baggers@cave>
2017-02-09 17:17:33 +0100

    fix 'van der corput radical-inverse'

--- gpu/misc.lisp
+++ gpu/misc.lisp
@@ -7,14 +7,14 @@
 
   vdc stand for Van Der Corput. For more details see:
   http://holger.dammertz.org/stuff/notes_HammersleyOnHemisphere.html"
-  (let ((bits (bit-ior (<< bits (uint 16)) (>> bits (uint 16))))
-	(bits (bit-ior (<< (bit-and bits (uint #x55555555)) (uint 1))
-		       (>> (bit-and bits (uint #xAAAAAAAA)) (uint 1))))
-	(bits (bit-ior (<< (bit-and bits (uint #x33333333)) (uint 2))
-		       (>> (bit-and bits (uint #xCCCCCCCC)) (uint 2))))
-	(bits (bit-ior (<< (bit-and bits (uint #x0F0F0F0F)) (uint 4))
-		       (>> (bit-and bits (uint #xF0F0F0F0)) (uint 4))))
-	(bits (bit-ior (<< (bit-and bits (uint #x00FF00FF)) (uint 8))
-		       (>> (bit-and bits (uint #xFF00FF00)) (uint 8)))))
-    ;;                  ↓ 0x100000000 ↓
-    (* (float bits) 2.3283064365386963e-10)))
+  (let* ((bits (bit-ior (<< bits (uint 16)) (>> bits (uint 16))))
+         (bits (bit-ior (<< (bit-and bits (uint #x55555555)) (uint 1))
+                        (>> (bit-and bits (uint #xAAAAAAAA)) (uint 1))))
+         (bits (bit-ior (<< (bit-and bits (uint #x33333333)) (uint 2))
+                        (>> (bit-and bits (uint #xCCCCCCCC)) (uint 2))))
+         (bits (bit-ior (<< (bit-and bits (uint #x0F0F0F0F)) (uint 4))
+                        (>> (bit-and bits (uint #xF0F0F0F0)) (uint 4))))
+         (bits (bit-ior (<< (bit-and bits (uint #x00FF00FF)) (uint 8))
+                        (>> (bit-and bits (uint #xFF00FF00)) (uint 8)))))
+    ;;                              ↓ 0x100000000 as a float ↓
+    (* (float bits) (glsl-expr "2.3283064365386963e-10" :float))))
