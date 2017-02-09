51bb36194ed0365a2fa34264238066ef3835878f
baggers <baggers@cave>
2017-02-09 17:18:36 +0100

    normalize all results from uv->cube-map-directions

--- gpu/sampling.lisp
+++ gpu/sampling.lisp
@@ -9,11 +9,11 @@
 
 (defun-g uv->cube-map-directions ((uv :vec2))
   (let ((scaled-uv (v! (* (- (x uv) 0.5) 2)
-		       (* (- (y uv) 0.5) 2 -1s0))))
+                       (* (- (y uv) 0.5) 2 -1s0))))
     (values
-     (v! 1s0 (y scaled-uv) (- (x scaled-uv)))
-     (v! -1s0 (y scaled-uv) (x scaled-uv))
-     (v! (x scaled-uv) 1s0 (- (y scaled-uv)))
-     (v! (x scaled-uv) -1s0 (y scaled-uv))
-     (v! (x scaled-uv) (y scaled-uv) 1s0)
-     (v! (- (x scaled-uv)) (y scaled-uv) -1s0))))
+     (normalize (v! 1s0 (y scaled-uv) (- (x scaled-uv))))
+     (normalize (v! -1s0 (y scaled-uv) (x scaled-uv)))
+     (normalize (v! (x scaled-uv) 1s0 (- (y scaled-uv))))
+     (normalize (v! (x scaled-uv) -1s0 (y scaled-uv)))
+     (normalize (v! (x scaled-uv) (y scaled-uv) 1s0))
+     (normalize (v! (- (x scaled-uv)) (y scaled-uv) -1s0)))))
