dec397b17fab367a55611dac6c7d79528c136bbb
Baggers <chris.bagley@gmail.com>
2017-06-13 00:33:47 +0200

    handle errors in init for def-simple-main-loop

--- misc.lisp
+++ misc.lisp
@@ -18,28 +18,29 @@
                 (progn
                   (setf ,frame-var-name (or frames -1))
                   (format t "~%- starting ~a -" ',name)
-                  (unless cepl.context::*gl-context*
-                    (cepl:repl))
-                  (let ((on-start ,on-start))
-                    (when on-start
-                      (funcall on-start)))
                   (unwind-protect
-                       (loop :until (= ,frame-var-name 0) :do
-                          (progn
-                            (decf ,frame-var-name 1)
-                            ;; update swank
-                            (livesupport:continuable
-                              (livesupport:update-repl-link))
-                            ;; update event system
-                            (livesupport:continuable
-                              (cepl:step-host))
-                            ;; update temporal pool
-                            ,(when (find-package :temporal-functions)
-                                   `(livesupport:continuable
-                                      (,(intern "UPDATE" :ttm))))
-                            ;; run step function
-                            (livesupport:continuable
-                              (,step-func-name))))
+                       (progn
+                         (unless cepl.context::*gl-context*
+                           (cepl:repl))
+                         (let ((on-start ,on-start))
+                           (when on-start
+                             (funcall on-start)))
+                         (loop :until (= ,frame-var-name 0) :do
+                            (progn
+                              (decf ,frame-var-name 1)
+                              ;; update swank
+                              (livesupport:continuable
+                                (livesupport:update-repl-link))
+                              ;; update event system
+                              (livesupport:continuable
+                                (cepl:step-host))
+                              ;; update temporal pool
+                              ,(when (find-package :temporal-functions)
+                                     `(livesupport:continuable
+                                        (,(intern "UPDATE" :ttm))))
+                              ;; run step function
+                              (livesupport:continuable
+                                (,step-func-name)))))
                     (setf ,frame-var-name 0)
                     (format t "~%~%- stopping ~a -~%" ',name)))
                 (format t "~%~%- ~a is already running -~%" ',name)))
