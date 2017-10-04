4fcf3bad9ae8f5ebf39e0612efb82fc52af8f799
cbaggers <chris.bagley@gmail.com>
2017-10-04 22:08:20 +0200

    another calc-normals func

--- normals/calculate-normals.lisp
+++ normals/calculate-normals.lisp
@@ -17,6 +17,23 @@
          1f0
          (* scale (- (- s6 (- (+ s0 (+ (* 2 (- s7 s1)) s8)) s2))))))))
 
+(defun-g simple-sample-normals ((func (function (:vec2) :float))
+                                (pos :vec2)
+                                (offset :vec2))
+  (let ((scale 1f0)
+        (s0 (funcall func (+ pos (v! (- (x offset))  (- (y offset))))))
+        (s1 (funcall func (+ pos (v!             .0  (- (y offset))))))
+        (s2 (funcall func (+ pos (v!     (x offset)  (- (y offset))))))
+        (s3 (funcall func (+ pos (v! (- (x offset)) .0))))
+        (s5 (funcall func (+ pos (v!     (x offset) .0))))
+        (s6 (funcall func (+ pos (v! (- (x offset))  (y offset)))))
+        (s7 (funcall func (+ pos (v!             .0  (y offset)))))
+        (s8 (funcall func (+ pos (v!     (x offset)  (y offset))))))
+    (normalize
+     (v! (* scale (- (- s2 (- (+ s0 (+ (* 2 (- s5 s3)) s8)) s6))))
+         1f0
+         (* scale (- (- s6 (- (+ s0 (+ (* 2 (- s7 s1)) s8)) s2))))))))
+
 
 (defun-g simple-sample-normals ((func (function (:vec3) :float))
                                 (pos :vec3)
