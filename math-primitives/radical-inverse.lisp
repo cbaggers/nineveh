3e0057842904576e5a3b8ea8ff8857e5c4016248
cbaggers <chris.bagley@gmail.com>
2018-03-24 01:37:16 +0100

    bit-* functions were incorrect, move to log* functions

--- math-primitives/radical-inverse.lisp
+++ math-primitives/radical-inverse.lisp
@@ -7,14 +7,14 @@
 
   vdc stand for Van Der Corput. For more details see:
   http://holger.dammertz.org/stuff/notes_HammersleyOnHemisphere.html"
-  (let* ((bits (bit-ior (<< bits (uint 16)) (>> bits (uint 16))))
-         (bits (bit-ior (<< (bit-and bits (uint #x55555555)) (uint 1))
-                        (>> (bit-and bits (uint #xAAAAAAAA)) (uint 1))))
-         (bits (bit-ior (<< (bit-and bits (uint #x33333333)) (uint 2))
-                        (>> (bit-and bits (uint #xCCCCCCCC)) (uint 2))))
-         (bits (bit-ior (<< (bit-and bits (uint #x0F0F0F0F)) (uint 4))
-                        (>> (bit-and bits (uint #xF0F0F0F0)) (uint 4))))
-         (bits (bit-ior (<< (bit-and bits (uint #x00FF00FF)) (uint 8))
-                        (>> (bit-and bits (uint #xFF00FF00)) (uint 8)))))
+  (let* ((bits (logior (<< bits (uint 16)) (>> bits (uint 16))))
+         (bits (logior (<< (logand bits (uint #x55555555)) (uint 1))
+                       (>> (logand bits (uint #xAAAAAAAA)) (uint 1))))
+         (bits (logior (<< (logand bits (uint #x33333333)) (uint 2))
+                       (>> (logand bits (uint #xCCCCCCCC)) (uint 2))))
+         (bits (logior (<< (logand bits (uint #x0F0F0F0F)) (uint 4))
+                       (>> (logand bits (uint #xF0F0F0F0)) (uint 4))))
+         (bits (logior (<< (logand bits (uint #x00FF00FF)) (uint 8))
+                       (>> (logand bits (uint #xFF00FF00)) (uint 8)))))
     ;;                              ↓ 0x100000000 as a float ↓
     (* (float bits) (glsl-expr "2.3283064365386963e-10" :float))))
