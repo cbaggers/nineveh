408fc6357d8bb00bd7c091beb7be3082b6ab6225
cbaggers <chris.bagley@gmail.com>
2018-04-02 16:38:28 +0200

    normalize max-contrast & rarely-confused color sets

--- color/sets.lisp
+++ color/sets.lisp
@@ -1,36 +1,36 @@
 (in-package :nineveh.color)
 
 (defvar *boytons-11-rarely-confused-colors*
-  (list (v! 0 0 255)      ;;Blue
-        (v! 255 0 0)      ;;Red
-        (v! 0 255 0)      ;;Green
-        (v! 255 255 0)    ;;Yellow
-        (v! 255 0 255)    ;;Magenta
-        (v! 255 128 128)  ;;Pink
-        (v! 128 128 128)  ;;Gray
-        (v! 128 0 0)      ;;Brown
-        (v! 255 128 0)))  ;;Orange
+  (list (v! 0.0 0.0 1.0)  ;;Blue
+        (v! 1.0 0.0 0.0)  ;;Red
+        (v! 0.0 1.0 0.0)  ;;Green
+        (v! 1.0 1.0 0.0)  ;;Yellow
+        (v! 1.0 0.0 1.0) ;;Magenta
+        (v! 1.0 0.5019608 0.5019608)  ;;Pink
+        (v! 0.5019608 0.5019608 0.5019608) ;;Gray
+        (v! 0.5019608 0.0 0.0)  ;;Brown
+        (v! 1.0 0.5019608 0.0))) ;;Orange
 
 (defvar *kellys-max-contrast-colors*
-  (list (v! #xFF #xB3 #x00)   ;;Vivid Yellow
-        (v! #x80 #x3E #x75) ;;Strong Purple
-        (v! #xFF #x68 #x00) ;;Vivid Orange
-        (v! #xA6 #xBD #xD7) ;;Very Light Blue
-        (v! #xC1 #x00 #x20) ;;Vivid Red
-        (v! #xCE #xA2 #x62) ;;Grayish Yellow
-        (v! #x81 #x70 #x66) ;;Medium Gray
+  (list (v! 1.0 0.7019608 0.0) ;;Vivid Yellow
+        (v! 0.5019608 0.24313727 0.45882356) ;;Strong Purple
+        (v! 1.0 0.40784317 0.0) ;;Vivid Orange
+        (v! 0.6509804 0.7411765 0.8431373) ;;Very Light Blue
+        (v! 0.7568628 0.0 0.1254902) ;;Vivid Red
+        (v! 0.8078432 0.63529414 0.38431376) ;;Grayish Yellow
+        (v! 0.5058824 0.43921572 0.40000004) ;;Medium Gray
 
         ;;The following will not be good for people with defective color vision
-        (v! #x00 #x7D #x34) ;;Vivid Green
-        (v! #xF6 #x76 #x8E) ;;Strong Purplish Pink
-        (v! #x00 #x53 #x8A) ;;Strong Blue
-        (v! #xFF #x7A #x5C) ;;Strong Yellowish Pink
-        (v! #x53 #x37 #x7A) ;;Strong Violet
-        (v! #xFF #x8E #x00) ;;Vivid Orange Yellow
-        (v! #xB3 #x28 #x51) ;;Strong Purplish Red
-        (v! #xF4 #xC8 #x00) ;;Vivid Greenish Yellow
-        (v! #x7F #x18 #x0D) ;;Strong Reddish Brown
-        (v! #x93 #xAA #x00) ;;Vivid Yellowish Green
-        (v! #x59 #x33 #x15) ;;Deep Yellowish Brown
-        (v! #xF1 #x3A #x13) ;;Vivid Reddish Orange
-        (v! #x23 #x2C #x16))) ;;Dark Olive Green
+        (v! 0.0 0.4901961 0.20392159) ;;Vivid Green
+        (v! 0.96470594 0.46274513 0.5568628) ;;Strong Purplish Pink
+        (v! 0.0 0.3254902 0.5411765) ;;Strong Blue
+        (v! 1.0 0.4784314 0.36078432) ;;Strong Yellowish Pink
+        (v! 0.3254902 0.21568629 0.4784314) ;;Strong Violet
+        (v! 1.0 0.5568628 0.0) ;;Vivid Orange Yellow
+        (v! 0.7019608 0.15686275 0.31764707) ;;Strong Purplish Red
+        (v! 0.9568628 0.7843138 0.0) ;;Vivid Greenish Yellow
+        (v! 0.49803925 0.09411766 0.050980397) ;;Strong Reddish Brown
+        (v! 0.5764706 0.6666667 0.0) ;;Vivid Yellowish Green
+        (v! 0.34901962 0.20000002 0.08235294) ;;Deep Yellowish Brown
+        (v! 0.9450981 0.227451 0.07450981) ;;Vivid Reddish Orange
+        (v! 0.13725491 0.17254902 0.08627451))) ;;Dark Olive Green
